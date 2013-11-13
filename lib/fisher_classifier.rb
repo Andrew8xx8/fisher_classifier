78debb092298bc10674498d9c90a0aaff896715f
Andreww8xx8 <avk@8xx8.ru>
2013-11-13 18:50:11 +0400

    In-memory classifier moved to lib

--- lib/fisher_classifier.rb
+++ lib/fisher_classifier.rb
@@ -10,5 +10,52 @@ module FisherClassifier
 
       Classifier.new config
     end
+
+    def create_in_memory
+      create do
+        @features = {}
+        @categories = {}
+
+        inc_feature do |feature, category|
+          @features[category] ||= {}
+
+          if @features[category].has_key? feature
+            @features[category][feature] += 1
+          else
+            @features[category][feature] = 1
+          end
+        end
+
+        inc_category do |category|
+          if @categories.has_key? category
+            @categories[category] += 1
+          else
+            @categories[category] = 1
+          end
+        end
+
+        get_features do |text|
+          text.split(' ')
+        end
+
+        categories do
+          [:good, :bad]
+        end
+
+        category_count do |category|
+          @categories[category] || 0
+        end
+
+        features_count do |feature, category|
+          @features[category][feature] || 0
+        end
+
+        default_category do
+          :none
+        end
+
+      end
+
+    end
   end
 end
--- spec/fisher_classifier_spec.rb
+++ spec/fisher_classifier_spec.rb
@@ -2,50 +2,8 @@ require 'spec_helper'
 
 describe FisherClassifier do
   before do
-    # In-memory classifier
-    @classifier = FisherClassifier.create do
-      @features = {}
-      @categories = {}
-
-      inc_feature do |feature, category|
-        @features[category] ||= {}
-
-        if @features[category].has_key? feature
-          @features[category][feature] += 1
-        else
-          @features[category][feature] = 1
-        end
-      end
-
-      inc_category do |category|
-        if @categories.has_key? category
-          @categories[category] += 1
-        else
-          @categories[category] = 1
-        end
-      end
-
-      get_features do |text|
-        text.split(' ')
-      end
-
-      categories do
-        [:good, :bad]
-      end
-
-      category_count do |category|
-        @categories[category] || 0
-      end
-
-      features_count do |feature, category|
-        @features[category][feature] || 0
-      end
-
-      default_category do
-        :none
-      end
-    end
-
+    # Use in-memory classifier
+    @classifier = FisherClassifier.create_in_memory
   end
 
   it 'should calculate feature prob' do
