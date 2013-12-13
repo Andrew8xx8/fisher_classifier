35a1d4c2a18c6208bb08e3a6b6ab3df87d44142b
Andreww8xx8 <avk@8xx8.ru>
2013-12-13 15:02:44 +0400

    metadata format changed

--- lib/fisher_classifier/meta.rb
+++ lib/fisher_classifier/meta.rb
@@ -7,56 +7,45 @@ module FisherClassifier
         text: text,
         default_category: default_category,
         selected_category: classify(text),
-        features: features,
-        features_meta: meta_features(features),
-        categories: categories,
-        categories_meta: meta_categories(features),
+        features: meta_features(features),
+        categories: meta_categories(features),
       }
     end
 
     private
 
     def meta_categories(features)
-      cats = {}
-
       categories.map do |category|
         pm = probs_multiply(features, category)
-        cats[category] = {
+        {
+          name: category,
           probs_multiply: pm,
           fisher_factor: fisher_factor(pm),
           fisher_prob: fisher_prob(category, features),
         }
       end
-
-      cats
     end
 
     def meta_features(features)
-      feats = {}
-
       features.map do |feature|
-        feats[feature] =  {
+        {
           name: feature,
           feature_in_all_categories: feature_in_all_categories(feature),
           categories: meta_feature_categories(feature)
         }
       end
-
-      feats
     end
 
     def meta_feature_categories(feature)
-      cats = {}
       categories.map do |category|
-        cats[category] = {
+        {
+          name: category,
           category_prob: category_prob(feature, category),
           feature_prob: feature_prob(feature, category),
           weighted_prob: weighted_prob(feature, category),
           freqsum: feature_freqsum(feature, category)
         }
       end
-
-      cats
     end
   end
 end
